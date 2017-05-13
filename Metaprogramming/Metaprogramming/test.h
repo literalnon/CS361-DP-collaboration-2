5d120a268a5118b52be82af43e7e29dc52c9f1ed
literalnon <donaldjrduck@gmail.com>
2017-05-13 11:43:35 +0300

    change enter constant

---
+++ Metaprogramming/Metaprogramming/test.h
@@ -0,0 +1,57 @@
+
+#include <iomanip>
+
+struct Test
+{
+  int m_n;
+  int m_nCheck;
+  bool m_bThrowLater;
+  static bool ms_bThrowDeferred;
+  //
+  Test(int n, bool bThrow = false, bool bThrowLater = false)
+    : m_n(n),
+      m_nCheck(1),
+      m_bThrowLater(bThrowLater)
+  {
+    cout << "constructor Test(" << setw(3) << n << ")" << endl;
+    if (bThrow)
+    {
+      cout << "Throwing..." << endl;
+      throw std::logic_error(string("logic_exception Test(") + to_string(n) + ")");
+    }
+  }
+  //
+  ~Test(){
+      cout << "destructor\n"<<endl;
+  }
+
+  Test(Test &&rrTest)
+    : m_n(rrTest.m_n),
+      m_nCheck(rrTest.m_nCheck),
+      m_bThrowLater(rrTest.m_bThrowLater)
+  {
+    cout << "Test(Test &&rrTest), rrTest == " << rrTest << endl;
+    if (m_bThrowLater && ms_bThrowDeferred)
+    {
+      cout << "Throwing..." << endl;
+      throw std::logic_error("Test(Test &&)");
+    }
+  }
+  //
+  friend std::ostream &operator << (std::ostream &rStream, const Test &rcTest)
+  {
+    if (rcTest.m_nCheck == 1)
+      rStream << setw(3) << rcTest.m_n;
+    else
+      rStream << "invalid";
+    //
+    return rStream;
+  }
+  //
+  Test(const Test &) = delete;
+  Test &operator = (const Test &) = delete;
+  Test &operator = (Test &&) = delete;
+};
+
+bool Test::ms_bThrowDeferred = false;
+
